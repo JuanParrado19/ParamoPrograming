561ff0777c02b6839c8dd06dc8ba3218c842e741
César Felipe Giraldo Mora <cesarfelipe0316@gmail.com>
2025-01-30 14:29:55 -0500

    Añadido la animación al scroller

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,52 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-const Home = () => (
-  <section
-    id="hero"
-    className="flex items-center content-center justify-center"
-  >
-    <video className="w-[100] h-[100] z-0" autoPlay muted loop style={{ zIndex: "-1" }}>
-      <source src="src/assets/video/bgvideo.mp4" type="video/mp4" />
-    </video>
-    <div
-      className="absolute z-10 flex flex-col items-center justify-center"
-      style={{ padding: "5vh"}}
+const Home = () => {
+  const handleScroll = () => {
+    const element = document.getElementById("start");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <section
+      id="hero"
+      className="flex items-center justify-center h-screen relative"
     >
-      <h1 className="text-center linearGradient mb-0 NameLogo">
-        PÁRAMO PROGRAMING
-      </h1>
-      <p
-        className="text-center text-white"
+      <video
+        className="w-full h-full object-cover absolute z-0"
+        autoPlay
+        muted
+        loop
       >
-        ¡ Desarrollo de cima y a la medida !
-      </p>
-      <div className="mt-[3rem] animate-bounce">
-        <a href="#start">
-          <FontAwesomeIcon
-            icon="chevron-down"
-            size="2xl"
-            className=" "
-            style={{ color: "#ffffff" }}
-          />
-        </a>
+        <source src="src/assets/video/bgvideo.mp4" type="video/mp4" />
+      </video>
+
+      <div className="absolute z-10 flex flex-col items-center justify-center text-center">
+        <h1 className="text-center linearGradient mb-0 NameLogo">
+          PÁRAMO PROGRAMING
+        </h1>
+        <p className="text-center text-white mt-2">
+          ¡ Desarrollo de cima y a la medida !
+        </p>
+
+        <div className="mt-12 animate-bounce">
+          <button
+            onClick={handleScroll}
+            className="focus:outline-none cursor-pointer"
+          >
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              size="2xl"
+              className="text-white"
+            />
+          </button>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Home;
