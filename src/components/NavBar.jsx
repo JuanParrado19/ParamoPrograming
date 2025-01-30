f2d989baeabdb0c3a667f2f72184e964f9a0d8b5
IvanGutierrez10 <alejandroguti972@outlook.com>
2025-01-30 12:48:09 -0500

    Ajustes comentarios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,12 +4,12 @@ import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-white/50 backdrop-blur-md shadow-lg rounded-full px-3 py-2.5 z-50 w-[55%] flex items-center">
-      {/* Logo con margen reducido a la derecha */}
+      {/* Logo */}
       <Link to="/" className="flex-shrink-0 ml-7 mr-2">
         <img src="src/assets/Logo.png" className="h-8.5 w-auto" alt="Logo" />
       </Link>
 
-      {/* Menú de navegación centrado y con márgenes equilibrados */}
+      {/* Menú de navegación */}
       <ul className="flex flex-1 justify-between text-sm text-gray-800 mx-9">
         <li>
           <NavLink
