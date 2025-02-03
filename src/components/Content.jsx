7bb63156ee6860542b0c14e596fb4377fe260643
JuanParrado19 <juanaparrado19@gmail.com>
2025-02-02 19:27:27 -0500

    Agrega avances

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -18,64 +18,59 @@ const Content = () => {
   return (
     <section
       id="start"
-      className="py-5 align-items-center justify-content-center flex-row-reversed overflow-hidden"
+      className="flex flex-row"
       style={{
         background:
           "linear-gradient(180deg, #090D23 0%, #1D255C 17.63%, #2B3788 43.99%, #3948B3 69.5%, #4658DA 100%)",
       }}
     >
-      <div className="d-flex py-4">
-        <div>
-          <div>
-            <h5 className="text-white">
-              Creemos una imagen digital <u>unica</u> a tu negocio que te ayude
-              a crecer!
-            </h5>
-          </div>
-
-          <div>
-            <AccordionButton
-              label="Landing page"
-              isOpen={openIndex === 0}
-              onClick={() => handleToggle(0)}
-            >
-              <span className="bodytext text-white">
-                Es una herramienta esencial en el marketing digital diseñada
-                específicamente para convertir visitantes en clientes
-                potenciales.La idea principal es que des a conocer tus
-                productos/servicios.
-              </span>
-            </AccordionButton>
-            <br />
-            <AccordionButton
-              label="E-Commerce"
-              isOpen={openIndex === 1}
-              onClick={() => handleToggle(1)}
-            >
-              <span className="bodytext text-white">
-                Es una plataforma en línea diseñada para permitir a las empresas
-                vender productos y servicios a través de un portal unico
-                diseñado específicamente para su negocio. Su objetivo principal
-                es facilitar las transacciones y brindar una experiencia de
-                compra cómoda y segura para los clientes.
-              </span>
-            </AccordionButton>
-            <br />
-            <AccordionButton
-              label="Portafolios"
-              isOpen={openIndex === 2}
-              onClick={() => handleToggle(2)}
-            >
-              <span className="bodytext text-white">
-                Es una herramienta digital que permite a profesionales y
-                empresas mostrar sus trabajos, habilidades y proyectos
-                realizados. Su objetivo principal es destacar su experiencia y
-                atraer potenciales clientes o empleadores.
-              </span>
-            </AccordionButton>
-          </div>
+      <div className="flex flex-row">
+        <div className="flex flex-col py-4">
+          <h5 className="text-white">
+            Creemos una imagen digital <u>unica</u> a tu negocio que te ayude
+            a crecer!
+          </h5>
+          <AccordionButton
+            label="Landing page"
+            isOpen={openIndex === 0}
+            onClick={() => handleToggle(0)}
+          >
+            <span className="bodytext text-white">
+              Es una herramienta esencial en el marketing digital diseñada
+              específicamente para convertir visitantes en clientes
+              potenciales.La idea principal es que des a conocer tus
+              productos/servicios.
+            </span>
+          </AccordionButton>
+          <br />
+          <AccordionButton
+            label="E-Commerce"
+            isOpen={openIndex === 1}
+            onClick={() => handleToggle(1)}
+          >
+            <span className="bodytext text-white">
+              Es una plataforma en línea diseñada para permitir a las empresas
+              vender productos y servicios a través de un portal unico
+              diseñado específicamente para su negocio. Su objetivo principal
+              es facilitar las transacciones y brindar una experiencia de
+              compra cómoda y segura para los clientes.
+            </span>
+          </AccordionButton>
+          <br />
+          <AccordionButton
+            label="Portafolios"
+            isOpen={openIndex === 2}
+            onClick={() => handleToggle(2)}
+          >
+            <span className="bodytext text-white">
+              Es una herramienta digital que permite a profesionales y
+              empresas mostrar sus trabajos, habilidades y proyectos
+              realizados. Su objetivo principal es destacar su experiencia y
+              atraer potenciales clientes o empleadores.
+            </span>
+          </AccordionButton>
         </div>
-        <div>
+        <div className="flex flex-col">
           <div style={{ position: "relative" }}>
             <img
               src="./src/assets/Browser.png"
@@ -85,7 +80,6 @@ const Content = () => {
             />
             <div
               style={{
-                position: "absolute",
                 width: "92.8%",
                 height: "24.3vw",
                 top: "4vw",
@@ -97,7 +91,7 @@ const Content = () => {
             >
               <img
                 src={images[openIndex]}
-                className="img-fluid"
+                className="flex"
                 alt="Content"
                 style={{
                   width: "100%",
