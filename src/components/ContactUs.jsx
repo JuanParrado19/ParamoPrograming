d940f67c3995b02bcd06773ab3f6e83ad539b12e
JuanParrado19 <99045699+JuanParrado19@users.noreply.github.com>
2025-02-06 17:38:19 -0500

    Feat/param 23 (#16)
    * cambios
    
    * Agrega formulario

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -6,22 +6,6 @@ const ContactUs = () => {
       <h4 className="bodytext text-white mb-4">
         ¡No te preocupes si no sabes por donde empezar!
       </h4>
-      <div className="flex flex-row justify-center m-4">
-        <button
-          style={{ width: "15vw" }}
-          className="custom-button text-white mr-2 flex items-center justify-center"
-          onClick={() => (window.location.href = "/contact")}
-        >
-          Contáctanos
-        </button>
-        <button
-          style={{ width: "15vw" }}
-          className="custom-button text-white flex items-center justify-center"
-          onClick={() => (window.location.href = "/servicios")}
-        >
-          Servicios
-        </button>
-      </div>
     </div>
   );
 };
