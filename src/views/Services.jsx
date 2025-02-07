5946a5cd830f90063c5058a5480c612c53316bc4
César Felipe Giraldo Mora <cesarfelipe0316@gmail.com>
2025-02-06 19:33:18 -0500

    prueba de service

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 import Service from "../components/Service";
 import PageTransitions from "../components/PageTransitions";
+
 const Services = () => (
   <PageTransitions>
-    <div className="d-block p-0 pt-5" style={{ maxWidth: "100%" }}>
+    <div className="container mx-auto px-4 mt-20">
       <Service
-        description="Our IT experts are delivering a range of IT services to our customers and alleviating technology
-                challenges regarding core IT infrastructure. We help you establish a reliable IT infrastructure."
-        image="./src/assets/Service.jpeg"
-        alignment="right"
-        title="Desarrollo de Software"
+        imageSrc="https://picsum.photos/800/800"
+        title="Desarrollo Web"
+        description="Desarrollamos páginas web y landing pages utilizando las tecnologías más modernas del mercado, enfocándonos en crear sitios rápidos, intuitivos y visualmente atractivos que ofrecen una experiencia de usuario excepcional."
+        imagePosition="left"
       />
+
       <Service
-        description="Al Sharq Technology provides you with all cyber security
-             services that secure your business, websites, and systems with high 
-             efficiency, raising the level of information security you have in work networks, systems, and websites."
-        image="./src/assets/Service2.jpeg"
-        alignment="left"
-        title="Paginas Web"
-      />
-      <Service
-        description="Our IT experts are delivering a range of IT services to our customers and alleviating technology challenges regarding core IT infrastructure. We help you establish a reliable IT infrastructure."
-        image="./src/assets/Service2.jpeg"
-        alignment="right"
-        title="Seguridad Informática"
+        imageSrc="https://picsum.photos/200/300"
+        title="Desarollo de Software"
+        description="Desarrollamos software empresarial ágil y escalable en busqueda de crear aplicaciones robustas y de alto rendimiento. Usamos de las mejores prácticas lo que nos permite ofrecer soluciones personalizadas que se adaptan a las necesidades específicas de tu negocio. "
+        imagePosition="right"
       />
     </div>
   </PageTransitions>
